350cebe22ede5b454f9d6bee696443ca368dd9da
Nardosha <lalabumlala@gmail.com>
2022-05-26 15:34:33 +0300

    refactor: refactoring

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,18 +6,25 @@ const destDir = path.join(__dirname, 'files-copy');
 const srcDir = path.join(__dirname, 'files');
 
 async function makeFolder() {
-  const data = await fsPromises.mkdir(destDir, {recursive: true});
-  console.log('data', data);
+  await fsPromises.mkdir(destDir, {recursive: true});
 
   const files = await fsPromises.readdir(srcDir, {withFileTypes: true});
-  console.log('files', files);
 
   for (const file of files) {
     const copyFileName = `copy-${file.name}`;
     const srcPath = path.join(srcDir, file.name);
     const destPath = path.join(destDir, copyFileName);
 
-    fs.copyFile(srcPath, destPath, err => console.log('jo',err));
+    fs.copyFile(srcPath, destPath, err => console.log(err));
+    checkUpdate(srcPath, destPath);
   }
 }
 makeFolder();
+
+function checkUpdate(pathSrc, destPath) {
+  const input = fs.createReadStream(pathSrc, 'utf-8');
+  const out = fs.createWriteStream(destPath);
+
+  input.pipe(out);
+}
+
