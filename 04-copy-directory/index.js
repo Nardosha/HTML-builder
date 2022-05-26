162082df206f54a5049e1c61c15c48937d15e6f8
Nardosha <lalabumlala@gmail.com>
2022-05-26 15:18:38 +0300

    feat: finish 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const fsPromises = require('node:fs/promises');
+const path = require('path');
 
+const destDir = path.join(__dirname, 'files-copy');
+const srcDir = path.join(__dirname, 'files');
+
+async function makeFolder() {
+  const data = await fsPromises.mkdir(destDir, {recursive: true});
+  console.log('data', data);
+
+  const files = await fsPromises.readdir(srcDir, {withFileTypes: true});
+  console.log('files', files);
+
+  for (const file of files) {
+    const copyFileName = `copy-${file.name}`;
+    const srcPath = path.join(srcDir, file.name);
+    const destPath = path.join(destDir, copyFileName);
+
+    fs.copyFile(srcPath, destPath, err => console.log('jo',err));
+  }
+}
+makeFolder();
